8d8453a379929d6169905fc63e330da54a09ea74
zeeeing <zjchew@singnet.com.sg>
2025-08-18 14:18:57 +0800

    Level-0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Buddy User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Buddy.java
@@ -0,0 +1,21 @@
+public class Buddy {
+    public static void main(String[] args) {
+        String logo = """
+                     ______     _    _    _____     _____    __   __
+                    |  ___ \\   | |  | |  |  __ \\   |  __ \\   \\ \\ / /
+                    | |___) |  | |  | |  | |  \\ \\  | |  \\ \\   \\ V /
+                    |  ___ \\   | |  | |  | |   | | | |   | |   | |  
+                    | |___) |  | |__| |  | |__/ /  | |__/ /    | |
+                    |______/    \\____/   |_____/   |_____/     |_|
+                """;
+        System.out.println("    Hello from\n" + logo);
+
+        String chatbotName = "Buddy";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
