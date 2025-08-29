b32b4ef15ddd74e8b3bf90d153b3719fa7745de7
zeeeing <zjchew@singnet.com.sg>
2025-08-29 13:37:43 +0800

    Add echo-ing feature

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Buddy {
     public static void main(String[] args) {
         String logo = """
@@ -8,14 +10,30 @@ public class Buddy {
                     | |___) |  | |__| |  | |__/ /  | |__/ /    | |
                     |______/    \\____/   |_____/   |_____/     |_|
                 """;
-        System.out.println("    Hello from\n" + logo);
-
         String chatbotName = "Buddy";
-        System.out.println("____________________________________________________________");
+
+        System.out.println("_________________________________________________________");
+        System.out.println("\n    Hello from\n" + logo);
+        System.out.println("_________________________________________________________");
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("_________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.trim().equalsIgnoreCase("bye")) {
+                System.out.println("_________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("_________________________________________________________");
+                break;
+            }
+
+            System.out.println("_________________________________________________________");
+            System.out.println("  " + input);
+            System.out.println("_________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
